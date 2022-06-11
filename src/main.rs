24d0bb51dd4eeca9a29d39fa80b1a792db1e76ce
kpflugshaupt <kpflugshaupt@gmail.com>
2022-06-11 13:14:13 +0200

    NAme parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let name = std::env::args()
+        .skip(1)
+        .next()
+        .unwrap_or_else(|| "world".to_string());
+
+    println!("Hello, {name}!");
 }
